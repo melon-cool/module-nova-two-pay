c1a9d3d11e26c943dfa320b6adbdcd98aa78d347
ike_liu <shuai.liu@silverexpress.asia>
2024-05-16 15:20:32 +0800

    commit

---
+++ Controller/Index/Test.php
@@ -0,0 +1,27 @@
+<?php
+namespace Magento\NovaTwoPay\Controller\Index;
+
+class Test extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		echo "Hello World";
+		//exit;
+	}
+
+	public function mini_see ()
+	{
+		echo 'My`name is your daye';
+		exit;
+	}
+}
